ad6f43cf07e4b1b8240fdf3f4b4fd73f8a94a679
Aheal <alejandroalamina@hotmail.com>
2017-08-09 20:29:56 -0500

    Se agrego la clase cart, client e items

--- ShoppinCar/src/shoppincar/Items.java
+++ ShoppinCar/src/shoppincar/Items.java
@@ -5,10 +5,41 @@
  */
 package shoppincar;
 
+import java.sql.*;
+
 /**
  *
  * @author aleja
  */
-public class Items {
+public class Items 
+{
+    Integer id, cantidadTotal, cantidadNinos, cantidadAdultos, precioTotal; 
+    String nombreTour; 
+    Date fecha; 
+    Time hora;
     
+    public void setId(Integer identificacion) 
+    {
+        this.id = identificacion; 
+    } 
+    public Integer getId() 
+    {
+        return this.id;
+    } 
+    public void setCantidad(Integer cantidadTotal) 
+    {
+        this.cantidadTotal = cantidadTotal; 
+    } 
+    public Integer getCantidad() 
+    {
+        return this.cantidadTotal;
+    }
+    public void setNinos(Integer ninos) 
+    {
+        this.cantidadNinos = ninos; 
+    } 
+    public Integer getNinos() 
+    {
+        return this.cantidadNinos;
+    }
 }
